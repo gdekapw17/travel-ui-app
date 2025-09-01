690275f3573a2cdd3cde77bbb883a603d748e025
Eka Purwadi <gdeka1734@gmail.com>
2025-09-01 13:18:39 +0800

    feat: finalize hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Button from "./Button";
 
 export default function Hero() {
   return (
-    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-400">
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 lg:flex-row">
       <div className="hero-map"></div>
 
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
@@ -37,6 +38,38 @@ export default function Hero() {
             271k{" "}
             <span className="regular-16 lg:regular-20">Excelent Review</span>
           </p>
+        </div>
+
+        <div className="flex flex-col w-full md:flex-row">
+          <Button title="Download App" variant="btn_green" type="button" />
+          <Button
+            title="How We Work"
+            variant="btn_white_text"
+            type="button"
+            icon="/play.svg"
+          />
+        </div>
+      </div>
+
+      <div className="relative flex flex-1 items-start">
+        <div className="relative z-20 w-[268px] flex flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+          <div className="flex flex-col">
+            <div className="flexBetween">
+              <p className="regular-16 text-gray-20">Location</p>
+              <Image src="/close.svg" alt="close" width={24} height={24} />
+            </div>
+            <p className="bold-20 text-white">Jawa Barat</p>
+          </div>
+          <div className="flexBetween">
+            <div className="flex flex-col">
+              <p className="regular-16 text-gray-20">Distance</p>
+              <p className="bold-20 text-white">1.312 Km</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="regular-16 text-gray-20">Elevation</p>
+              <p className="bold-20 text-white">6.967 Km</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
