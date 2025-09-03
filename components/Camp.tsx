bb3a01b92b9154a5db0274c9e4c3aa5a1f63ef3f
Eka Purwadi <gdeka1734@gmail.com>
2025-09-03 17:23:27 +0800

    feat: add more struture to camp section

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -2,8 +2,15 @@ import CampsiteContainer from "./CampsiteContainer";
 
 export default function Camp() {
   return (
-    <section className="xl:max-container relative flex flex-col py-10 lg:py-20 lg:mb-10 xl:mb-20 ">
+    <section className="2xl:max-container relative flex flex-col py-10 lg:px-20 lg:py-20 lg:mb-10 xl:mb-20">
       <CampsiteContainer />
+      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] rounded-3xl overflow-hidden w-full relative xl:rounded-5xl xl:px-16 xl:py-20">
+          <h2>
+            <strong>Feeling Lost</strong> And Not Knowing The Way?
+          </h2>
+        </div>
+      </div>
     </section>
   );
 }
--- components/CampsiteContainer.tsx
+++ components/CampsiteContainer.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useRef } from "react";
+import Image from "next/image";
+import { PEOPLE_URL } from "@/constants";
 
 type CampsiteProps = {
   backgroundImage: string;
@@ -16,7 +18,41 @@ function Campsite({
   peopleJoined,
 }: CampsiteProps) {
   return (
-    <div className={`h-full w-full min-w-[1100px] ${backgroundImage}`}></div>
+    <div
+      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-5xl`}
+    >
+      <div className="flex flex-col h-full items-start justify-between p-6 lg:px-20 lg:py-10">
+        <div className="flexCenter gap-4">
+          <div className="rounded-full p-4 bg-green-50">
+            <Image
+              src="folded-map.svg"
+              alt="folded-map"
+              width={28}
+              height={28}
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <h4 className="bold-18 text-white">{title}</h4>
+            <p className="regular-14 text-white">{subtitle}</p>
+          </div>
+        </div>
+
+        <div className="flexCenter gap-6">
+          <span className="flex -space-x-4">
+            {PEOPLE_URL.map((url, index) => (
+              <Image
+                src={url}
+                alt="people"
+                width={52}
+                height={52}
+                key={index}
+              />
+            ))}
+          </span>
+          <p className="bold-16 lg:bold-20 text-white">{peopleJoined}</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
@@ -46,31 +82,28 @@ export default function CampsiteContainer() {
   };
 
   return (
-    <div className="relative">
-      <div
-        ref={scrollRef}
-        className="
-            overflow-x-auto overflow-y-hidden hide-scrollbar
+    <div
+      ref={scrollRef}
+      className="
+            overflow-x-auto hide-scrollbar
             cursor-grab active:cursor-grabbing scroll-smooth
+            flex h-[340px] w-full gap-8 lg:h-[400px] xl:h-[600px]
           "
-        onMouseDown={handleMouseDown}
-      >
-        <div className="flex h-[340px] w-full items-start justify-start gap-8 lg:h-[400px] xl:h-[600px]">
-          <Campsite
-            backgroundImage="bg-bg-img-1"
-            title="Monkey Forest Camp"
-            subtitle="Ubud, Gianyar"
-            peopleJoined="50+ Joined"
-          />
+      onMouseDown={handleMouseDown}
+    >
+      <Campsite
+        backgroundImage="bg-bg-img-1"
+        title="Monkey Forest Camp"
+        subtitle="Ubud, Gianyar"
+        peopleJoined="50+ Joined"
+      />
 
-          <Campsite
-            backgroundImage="bg-bg-img-1"
-            title="Monkey Forest Camp"
-            subtitle="Ubud, Gianyar"
-            peopleJoined="50+ Joined"
-          />
-        </div>
-      </div>
+      <Campsite
+        backgroundImage="bg-bg-img-2"
+        title="Mountain View Camp"
+        subtitle="Somewhere in the wilderness"
+        peopleJoined="70+ Joined"
+      />
     </div>
   );
 }
