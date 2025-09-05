bd376f7f74c0fafad28cdf7722a8bf4825d51d6e
Eka Purwadi <gdeka1734@gmail.com>
2025-09-05 18:42:30 +0800

    feat: add base structure for feature page

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,3 +1,7 @@
 export default function Features() {
-  return <div>Features</div>;
+  return (
+    <section className="bg-feature-bg bg-center flexCenter py-24 bg-no-repeat flex-col">
+      <div className="max-container padding-container"></div>
+    </section>
+  );
 }
